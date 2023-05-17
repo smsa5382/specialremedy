f96ee8a499389cb00b07e161059842f5e3f71e67
smsa5382 <98916736+smsa5382@users.noreply.github.com>
2023-05-17 06:14:18 +0500

    Add files via upload

---
+++ Terms.php
@@ -0,0 +1,266 @@
+<!DOCTYPE html>
+<html style="font-size: 16px;" lang="en"><head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <meta name="keywords" content="">
+    <meta name="description" content="">
+    <title>Terms</title>
+    <link rel="stylesheet" href="nicepage.css" media="screen">
+<link rel="stylesheet" href="Terms.css" media="screen">
+    <script class="u-script" type="text/javascript" src="jquery.js" defer=""></script>
+    <script class="u-script" type="text/javascript" src="nicepage.js" defer=""></script>
+    <meta name="generator" content="Nicepage 5.9.10, nicepage.com">
+    <link rel="icon" href="images/Capture.PNG">
+    <link id="u-theme-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Open+Sans:300,300i,400,400i,500,500i,600,600i,700,700i,800,800i">
+    
+    
+    <script type="application/ld+json">{
+		"@context": "http://schema.org",
+		"@type": "Organization",
+		"name": "Special Health Remedies"
+}</script>
+    <meta name="theme-color" content="#478ac9">
+    <meta property="og:title" content="Terms">
+    <meta property="og:description" content="">
+    <meta property="og:type" content="website">
+  <meta data-intl-tel-input-cdn-path="intlTelInput/"></head>
+  <body class="u-body u-xl-mode" data-lang="en"><header class="u-clearfix u-header u-header" id="sec-e1f1"><div class="u-clearfix u-sheet u-valign-middle-sm u-valign-middle-xs u-sheet-1">
+        <nav class="u-menu u-menu-one-level u-offcanvas u-menu-1">
+          <div class="menu-collapse" style="font-size: 1rem; letter-spacing: 0px;">
+            <a class="u-button-style u-custom-color u-custom-left-right-menu-spacing u-custom-padding-bottom u-custom-text-active-color u-custom-text-hover-color u-custom-top-bottom-menu-spacing u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="#">
+              <svg class="u-svg-link" viewBox="0 0 24 24"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#menu-hamburger"></use></svg>
+              <svg class="u-svg-content" version="1.1" id="menu-hamburger" viewBox="0 0 16 16" x="0px" y="0px" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg"><g><rect y="1" width="16" height="2"></rect><rect y="7" width="16" height="2"></rect><rect y="13" width="16" height="2"></rect>
+</g></svg>
+            </a>
+          </div>
+          <div class="u-nav-container">
+            <ul class="u-nav u-unstyled u-nav-1"><li class="u-nav-item"><a class="u-button-style u-custom-color-1 u-nav-link u-text-active-white u-text-hover-grey-50" href="Home.html" style="padding: 10px 20px;">Home</a>
+</li><li class="u-nav-item"><a class="u-button-style u-custom-color-1 u-nav-link u-text-active-white u-text-hover-grey-50" href="Terms.html" style="padding: 10px 20px;">Terms</a>
+</li><li class="u-nav-item"><a class="u-button-style u-custom-color-1 u-nav-link u-text-active-white u-text-hover-grey-50" href="Privacy.html" style="padding: 10px 20px;">Privacy</a>
+</li><li class="u-nav-item"><a class="u-button-style u-custom-color-1 u-nav-link u-text-active-white u-text-hover-grey-50" href="DMCA.html" style="padding: 10px 20px;">DMCA</a>
+</li></ul>
+          </div>
+          <div class="u-nav-container-collapse">
+            <div class="u-black u-container-style u-inner-container-layout u-opacity u-opacity-95 u-sidenav">
+              <div class="u-inner-container-layout u-sidenav-overflow">
+                <div class="u-menu-close"></div>
+                <ul class="u-align-center u-nav u-popupmenu-items u-unstyled u-nav-2"><li class="u-nav-item"><a class="u-button-style u-nav-link" href="Home.html">Home</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link" href="Terms.html">Terms</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link" href="Privacy.html">Privacy</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link" href="DMCA.html">DMCA</a>
+</li></ul>
+              </div>
+            </div>
+            <div class="u-black u-menu-overlay u-opacity u-opacity-70"></div>
+          </div>
+        </nav>
+        <a class="u-active-none u-border-none u-btn u-button-link u-button-style u-hover-none u-none u-text-custom-color-1 u-btn-1" href="https://vclit.com/michael-toftif">100% Natural. Safe &amp; Secure.</a>
+      </div></header>
+    <section class="u-clearfix u-section-1" id="sec-4e18">
+      <div class="u-clearfix u-sheet u-sheet-1">
+        <p class="u-text u-text-default u-text-1"> Conditions of using our website <br>
+          <br>&nbsp; &nbsp;1. This
+ website promotes the business referred to on it. In these conditions, 
+the business will be referred to as ‘we’ and/or ’our’. <br>
+          <br>&nbsp; &nbsp;2. A wide range of intellectual property rights are used in and relating to this website, including:<br>
+          <br>a. the trademarks and logos;<br>
+          <br>b.
+ the design, text, graphics and other content of the web pages on this 
+website, together with all the web addresses associated with those web 
+pages; and <br>
+          <br>c. all the software used in relation to this website.<br>
+          <br>&nbsp; &nbsp;3. We
+ are the owner or the authorized licensee of these intellectual property
+ rights. You agree not to copy any content (including images) on this 
+website without our consent. <br>
+          <br>
+          <br> About these conditions <br>&nbsp; &nbsp;4. If
+ you access or use any part of this website you agree to these 
+conditions. If you do not want to agree to these conditions, do not 
+access or use this website.<br>
+          <br>&nbsp; &nbsp;5. We
+ may change these conditions at any time without giving you notice. 
+Please check these conditions from time to time for any changes. By 
+continuing to use the website you agree to all the changes we make to 
+these conditions. <br>
+          <br> Using this website <br>&nbsp; &nbsp;6. We collect and use 
+information in line with our . By using this website, you agree to the 
+way in which we collect and use your information.<br>
+          <br>&nbsp; &nbsp;7. You cannot use this website: <br>
+          <br>
+          <br>for any unlawful purpose; <br>
+          <br>
+          <br>to send spam; <br>
+          <br>
+          <br>to
+ harm, threaten, abuse or harass another person, or in a way that 
+invades someone's privacy or is (in our reasonable opinion) offensive or
+ is unacceptable or damaging to us, our customers or suppliers; <br>
+          <br>
+          <br>to
+ create, check, confirm, update or amend your own or someone else's 
+databases, records, directories, customer lists, mailing or prospecting 
+lists; <br>
+          <br>
+          <br>to tamper with, update or change any part of the website; <br>
+          <br>
+          <br>in a way that affects how it is run; <br>
+          <br>
+          <br>in
+ a way that imposes an unreasonable or disproportionably large burden on
+ our or our suppliers' communications and technical systems as 
+determined by us; or <br>
+          <br>
+          <br>using
+ any automated means to monitor or copy the website or its content, or 
+to interfere with or attempt to interfere with how the website works. <br>
+          <br>
+          <br> If you provide content for this website <br>&nbsp; &nbsp;8. If
+ you provide any material to this website (for example, by providing 
+ratings and reviews, comments, articles, or uploading any other content 
+in any format (including video)) (each “User Content”), you agree to 
+grant us permission, irrevocably and free of charge, to use User Content
+ (including altering and adapting it for operational or editorial 
+reasons) in any media worldwide, for our own marketing, research and 
+promotional activities and our internal business purposes which may 
+include providing the User Content to selected third party partners, 
+service providers, social media and networking sites. By posting ratings
+ and reviews, you agree to our . <br>
+          <br>&nbsp; &nbsp;9. You own your User Content at all times, and you continue to have the right to use it in any way you choose. <br>
+          <br>&nbsp; &nbsp;10. By providing any User Content to the Website you confirm that your User Content:<br>
+          <br> is
+ your own original work or you are authorized to provide it to the 
+Website and that you have the right to give us permission to use it for 
+the purposes set out in these terms; <br>
+          <br>
+          <br>will
+ not contain or promote anything illegal, harmful, misleading, abusive, 
+defamatory (that is, it does not damage someone's good reputation) or 
+anything else that might cause widespread offence or bring us or our 
+business partners into disrepute; <br>
+          <br>
+          <br>does not take away or affect any other person's privacy rights, contract rights or any other rights; <br>
+          <br>
+          <br>does
+ not contain any virus or other code that may damage, interfere with or 
+otherwise adversely affect the operation of the Website; <br>
+          <br>
+          <br>will,
+ if used to promote your own business or services, clearly and openly 
+state your association with the particular business expressly; <br>
+          <br>
+          <br>will not contain any form of mass-mailing or spam. <br>
+          <br>
+          <br> &nbsp; &nbsp;11. If you do not want to grant us the permissions set out above, please do not provide any material to the Website <br>
+          <br>&nbsp; &nbsp;12. We
+ have no obligation to publish your User Content on the Website and we 
+retain the right to remove any User Content at any time and for any 
+reason. <br>
+          <br>&nbsp; &nbsp;13. We
+ do not edit, pre-vet or review any User Content displayed on the 
+Website. If you believe that any User Content does not comply with the 
+requirements set out in this paragraph, please notify us immediately. We
+ will then review the User Content and, where we deem it appropriate, 
+remove it within a reasonable time. <br>
+          <br>
+          <br> If you send a message through this website <br>&nbsp; &nbsp;14. This
+ website is provided by Hibu Inc. ("Hibu"). Hibu shall have the right to
+ scan messages sent using the contact form on this website manually and 
+automatically and to store such messages. Any personal data you include 
+in such messages may be (a) used by Hibu for purposes reasonably 
+associated with provision of this website and services, (b) disclosed 
+where disclosure is required by law, and (c) used where any of your 
+actions have breached these Conditions of Use. Personal data may be used
+ by Hibu in an aggregated form as permitted by applicable law. <br>
+          <br>
+          <br> Disclaimers<br>
+          <br>&nbsp; &nbsp;15. You use the website at your own risk. <br>
+          <br>&nbsp; &nbsp;16. You should not rely on the website for advice. <br>
+          <br>&nbsp; &nbsp;17. As far as the relevant laws allow, we do not guarantee that: <br>
+          <br> there will be no problems with how you use the website; or <br>
+          <br>
+          <br>the computer or server you use to log on to the website is free of viruses or other harmful programs. <br>
+          <br>
+          <br>
+          <br> Limits to our liability <br>&nbsp; &nbsp;18. Under
+ no circumstances will we, the owner or operator of this website, or any
+ of their group companies, employees, officers or agents, or any other 
+organization involved in creating, producing, maintaining or 
+distributing the website be liable for any loss of: <br>
+          <br> profits; <br>
+          <br>
+          <br>business or business opportunities; <br>
+          <br>
+          <br>savings you expect to make; <br>
+          <br>
+          <br>goodwill; <br>
+          <br>
+          <br>use of, or corruption to information; or <br>
+          <br>
+          <br>information. <br>
+          <br>
+          <br> &nbsp; &nbsp;19. If
+ we do not keep to these conditions, they will only be liable for losses
+ you have suffered as a direct result. We are not liable to you for any 
+other losses whether such losses are because we have not kept to our 
+obligations or contract, because of something we have done or not done 
+in negligence, due to defamatory statements or liability for a product 
+or otherwise as a result of:<br>
+          <br> using or relying on the website; <br>
+          <br>
+          <br>not being able to use the website; <br>
+          <br>
+          <br>any
+ mistake, fault, failure to do something, missing information, or virus 
+on the website or if it does not work properly because of incidents 
+outside of our control such as (but not limited to) interruptions to 
+communication and networks and circumstances beyond our control; <br>
+          <br>
+          <br>theft, destruction of information or someone getting access to our records, programs or services without our permission; <br>
+          <br>
+          <br>goods, products, services or information received through or advertised on any website which we link to from this website; or <br>
+          <br>
+          <br>any
+ information, data, message or other material which you email, post, 
+upload, reproduce, send, or otherwise distribute or receive using the 
+website. <br>
+          <br>
+          <br>
+          <br> The whole agreement <br>&nbsp; 
+&nbsp;20. These
+ conditions make up the whole agreement between you and us in how you 
+use the website. If a court decides that a condition is not valid, the 
+rest of the conditions will still apply. <br>
+          <br>
+          <br> The law <br>&nbsp; &nbsp;21. The
+ laws of the state of New York (without reference to its conflict of 
+laws principles) apply to your use of the website and these conditions. 
+We control the website from within the United States. However, you can 
+get access to the website from other places around the world. Although, 
+these places may have different laws from the laws of the state of New 
+York, by using the website you agree that the laws of the state of New 
+York will apply to everything relating to you using the website and you 
+agree to keep to these laws. We have the right to take you to court in 
+the country and/ or state that you live in.<br>
+        </p>
+      </div>
+    </section>
+    
+    
+    <footer class="u-align-center u-clearfix u-footer u-grey-80 u-footer" id="sec-912c"><div class="u-clearfix u-sheet u-sheet-1">
+        <p class="u-small-text u-text u-text-variant u-text-1"> Copyright © 2023<br>
+        </p>
+      </div></footer>
+    <section class="u-backlink u-clearfix u-grey-80">
+      <a class="u-link" href="https://nicepage.com/website-templates" target="_blank">
+        <span>Website Templates</span>
+      </a>
+      <p class="u-text">
+        <span>created with</span>
+      </p>
+      <a class="u-link" href="" target="_blank">
+        <span>Website Builder Software</span>
+      </a>. 
+    </section>
+  
+</body></html>
